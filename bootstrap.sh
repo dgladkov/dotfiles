82d89e3bbd40938061e45bf6046e6dc418aac93b
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2013-02-28 12:18:04 +0200

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,30 @@ function doIt() {
 		rm -rf "$HOME/$i"
     	ln -s "$PWD/$i" "$HOME/$i"
 	done
+
+	local zsh=`grep -m 1 zsh /etc/shells`
+	if [[ "$SHELL" = *zsh ]]; then
+		`$zsh && source "$HOME/.zshrc"`
+	else
+		if [[ -n $zsh ]]; then
+			read -n 1 -p "Change your default shell to zsh? (y/n) "
+			echo
+			chsh -s /bin/sh
+			`$zsh`
+		else
+			echo "Please install zsh and make it default shell"
+		fi
+	fi
 }
 
 if [ "$1" = "--force" -o "$1" = "-f" ]; then
 	doIt
 else
-	read -n 1 -sp "This WILL overwrite existing files in your home directory. Are you sure? (y/n) "
+	read -n 1 -p "This WILL overwrite existing files in your home directory. Are you sure? (y/n) "
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
 fi
+
+unset doIt
