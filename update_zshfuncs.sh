0960d7e7a3a675fa7ddd6f77b3e3d40ff10475ab
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2013-10-09 11:16:26 +0300

    update zsh completions

--- update_zshfuncs.sh
+++ update_zshfuncs.sh
@@ -5,7 +5,7 @@ EXCLUDES=(_pip)
 for f in .zshfuncs/*; do
     filename=`basename $f`
     if [[ ! ${EXCLUDES[*]} =~ "$filename" ]]; then
-        echo wget -N --directory-prefix=./.zshfuncs "https://raw.github.com/zsh-users/zsh-completions/master/src/$filename"
+        wget -N --directory-prefix=./.zshfuncs "https://raw.github.com/zsh-users/zsh-completions/master/src/$filename"
     fi
 done
 
