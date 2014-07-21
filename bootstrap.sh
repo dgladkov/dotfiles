9712789d60238a3d44b07fe3bd1ab355b38c4980
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2014-07-21 13:55:50 +0300

    rsync indead of symlink bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
 
 function doIt() {
-	local IFS=$(echo -en "\n\b")
-	local files=`git ls-files`
-
-	for i in $files; do
-		if [[ $i == \.* ]]; then
-			# this is a hack to make bootstrap play nice with existing dirs
-			rm -rf "$HOME/$i"
-			mkdir -p "$HOME/$i"
-			rm -rf "$HOME/$i"
-			ln -s "$PWD/$i" "$HOME/$i"
-		fi
-	done
-
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	local zsh=`grep -m 1 zsh /etc/shells`
 	if [[ "$SHELL" = *zsh ]]; then
 		exec "$zsh"
@@ -27,17 +20,15 @@ function doIt() {
 			echo "Please install zsh package using your package manager"
 		fi
 	fi
-	make -C ./.vim/bundle/vimproc.vim
 }
 
-if [ "$1" = "--force" -o "$1" = "-f" ]; then
-	doIt
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
 else
-	read -n 1 -p "This WILL overwrite existing files in your home directory. Are you sure? (y/n) "
-	echo
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-
-unset doIt
+		doIt;
+	fi;
+fi;
+unset doIt;
