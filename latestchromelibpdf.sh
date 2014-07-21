5f460d94458d59f8d8bb0bba65fea16500dc78e3
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2014-07-21 17:22:06 +0300

    update libpdf fetcher for Arch

--- latestchromelibpdf.sh
+++ latestchromelibpdf.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-wget https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
-dpkg --fsys-tarfile google-chrome-unstable_current_amd64.deb | tar xfO - ./opt/google/chrome/libpdf.so | sudo tee /usr/lib/chromium-browser/libpdf.so > /dev/null
-sudo chown root:root /usr/lib/chromium-browser/libpdf.so
-sudo chmod 644 /usr/lib/chromium-browser/libpdf.so
-rm google-chrome-unstable_current_amd64.deb
+#!/bin/sh
+wget -nc https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
+ar x google-chrome-unstable_current_amd64.deb data.tar.lzma
+lzma -cd data.tar.lzma | tar xfO - ./opt/google/chrome-unstable/libpdf.so | sudo tee /usr/lib/chromium/libpdf.so > /dev/null
+sudo chown root:root /usr/lib/chromium/libpdf.so
+sudo chmod 644 /usr/lib/chromium/libpdf.so
+rm data.tar.lzma google-chrome-unstable_current_amd64.deb
